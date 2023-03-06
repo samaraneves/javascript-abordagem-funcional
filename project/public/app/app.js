a5fe1d783e768095c265b7fdb8f9a75567fada9b
Samara Neves <samara.neves@muralis.com.br>
2023-03-06 10:01:52 -0300

    criando functor

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,12 +1,13 @@
+import "./utils/array-helpers.js";
 import { handleStatus, log } from "./utils/promise-helpers.js";
 
-document
-.querySelector('#myButton')
-.onclick = () => 
-    fetch('http://localhost:3000/notas')
+document.querySelector("#myButton").onclick = () =>
+  fetch("http://localhost:3000/notas")
     .then(handleStatus)
-    .then(notas => notas.reduce((array, nota) => array.concat(nota?.itens), []))
-    .then(itensNota => itensNota.filter(item => item?.codigo == '2143'))
-    .then(itensFiltered => itensFiltered.reduce((total, item) => total + item?.valor, 0))
+    .then((notas) => notas.$flatMap((nota) => nota?.itens))
+    .then((itensNota) => itensNota.filter((item) => item?.codigo == "2143"))
+    .then((itensFiltered) =>
+      itensFiltered.reduce((total, item) => total + item?.valor, 0)
+    )
     .then(log)
-    .catch(log)
+    .catch(log);
