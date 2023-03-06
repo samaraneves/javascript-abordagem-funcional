ae1139fa375c0ad3fd065c4bccaf7c8dde5f9b8e
Samara Neves <sneves0@hotmail.com>
2023-03-05 21:55:40 -0300

    adicionando itens

---
+++ project/public/app/app.js
@@ -0,0 +1,12 @@
+import { handleStatus, log } from "./utils/promise-helpers.js";
+
+document
+.querySelector('#myButton')
+.onclick = () => 
+    fetch('http://localhost:3000/notas')
+    .then(handleStatus)
+    .then(notas => notas.reduce((array, nota) => array.concat(nota?.itens), []))
+    .then(itensNota => itensNota.filter(item => item?.codigo == '2143'))
+    .then(itensFiltered => itensFiltered.reduce((total, item) => total + item?.valor, 0))
+    .then(log)
+    .catch(log)
