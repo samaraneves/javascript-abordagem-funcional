bd84aa30996a8a2cc002f5dec43d97007c2c11b8
Samara Neves <sneves0@hotmail.com>
2023-03-14 20:46:48 -0300

    sobre functor

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,13 +1,14 @@
 import "./utils/array-helpers.js";
 import { handleStatus, log } from "./utils/promise-helpers.js";
 
+const sumItems = code => notas => 
+notas.$flatMap((nota) => nota?.itens)
+.filter((item) => item?.codigo == code)
+.reduce((total, item) => total + item?.valor, 0)
+
 document.querySelector("#myButton").onclick = () =>
   fetch("http://localhost:3000/notas")
     .then(handleStatus)
-    .then((notas) => notas.$flatMap((nota) => nota?.itens))
-    .then((itensNota) => itensNota.filter((item) => item?.codigo == "2143"))
-    .then((itensFiltered) =>
-      itensFiltered.reduce((total, item) => total + item?.valor, 0)
-    )
+    .then(sumItems('1'))
     .then(log)
     .catch(log);
