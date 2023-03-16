bbdfd7e43371ada1a331356d3e5f06c1b173921b
Samara Neves <sneves0@hotmail.com>
2023-03-16 20:31:21 -0300

    takeUntil, debounceTime, pipe

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,16 +1,21 @@
 import "./utils/array-helpers.js";
 import { log, logError } from "./utils/promise-helpers.js";
 import { notasService as service } from "./nota/service.js";
-import { takeUntil, debounceTime } from "./utils/operators.js";
+import { takeUntil, debounceTime, pipe, partialize } from "./utils/operators.js";
 
-const action = debounceTime(500, takeUntil(3, () => 
+const operations = pipe(
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 500),
+)
+
+const actions = operations(() => 
 service
     .sumItems('1')
     .then(log)
     .catch(logError)
-))
+)
 
 document
   .querySelector("#myButton")
-  .onclick = action
+  .onclick = actions
 
