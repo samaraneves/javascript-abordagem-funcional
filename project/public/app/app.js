410278c08417ddb970a01bc1c65c49b7c314b1d5
Samara Neves <sneves0@hotmail.com>
2023-03-16 19:42:19 -0300

    feat: debounceTime

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,16 +1,16 @@
 import "./utils/array-helpers.js";
 import { log, logError } from "./utils/promise-helpers.js";
 import { notasService as service } from "./nota/service.js";
-import { takeUntil } from "./utils/operators.js";
+import { takeUntil, debounceTime } from "./utils/operators.js";
 
-const operation = takeUntil(3, () => 
+const action = debounceTime(500, takeUntil(3, () => 
 service
     .sumItems('1')
     .then(log)
     .catch(logError)
-)
+))
 
 document
   .querySelector("#myButton")
-  .onclick = () => operation()
+  .onclick = action
 
