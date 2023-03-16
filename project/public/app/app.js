da427bf2228388c0cb645278a22a9dd26a0d5dbf
Samara Neves <sneves0@hotmail.com>
2023-03-16 19:26:32 -0300

    limitando operacoes

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,9 +1,16 @@
 import "./utils/array-helpers.js";
 import { log, logError } from "./utils/promise-helpers.js";
 import { notasService as service } from "./nota/service.js";
+import { takeUntil } from "./utils/operators.js";
 
-document.querySelector("#myButton").onclick = () =>
-  service
+const operation = takeUntil(3, () => 
+service
     .sumItems('1')
     .then(log)
-    .catch(logError);
+    .catch(logError)
+)
+
+document
+  .querySelector("#myButton")
+  .onclick = () => operation()
+
